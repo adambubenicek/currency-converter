2ef69435c5ff6658762d1377bac90c96e5c520b5
Adam Bubenicek <adam@adambubenicek.com>
2023-02-15 09:24:18 +0000

    Run prettier pre-commit

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,8 @@
-import { createServer } from "node:http"
+import { createServer } from "node:http";
 
 createServer(function (req, res) {
-  res.write("Hello, World!")  
-  res.end()
+  res.write("Hello, World!");
+  res.end();
 }).listen(3001, () => {
-  console.log("Starting server at the port 3001")
-})
-
+  console.log("Starting server at the port 3001");
+});
